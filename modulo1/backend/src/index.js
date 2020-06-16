3aafbb05ef8d6b4434d6a87d0b18796ec9de3450
Lucca <luccab.souza@gmail.com>
2020-06-16 20:18:00 -0300

    Acompanhamento do primeiro modulo: node.js

---
+++ modulo1/backend/src/index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+const port = 8081
+
+const projects = []
+let counter = 0
+
+function logRequest(req, res, next){
+    const { method, url} = req
+    const logLabel = `${method} - ${url}`
+    console.time(logLabel) // passo 1 
+    next() 
+    console.timeEnd(logLabel) 
+}
+
+app.use(express.json())
+app.use(logRequest)
+
+app.get('/projects', logRequest,  (req, res)=>{
+    const owner = req.query.owner
+
+    const FilteredProjects = owner ? projects.filter(project => project.owner.includes(owner)) : projects
+    return res.json(FilteredProjects)
+})
+
+app.post('/projects', (req, res)=>{
+    const data = req.body
+    const newproject = {
+        title: data.title,
+        owner: data.owner,
+        technologies: data.technologies,
+        id: counter
+    }
+    projects.push(newproject)
+    counter++
+    return res.json(newproject)
+
+})
+
+app.put('/projects/:id', (req, res)=>{
+    const ProjectIndex = projects.findIndex(project => project.id == req.params.id)
+    const data = req.body
+  
+    if(ProjectIndex == -1){
+        return res.status(400).json({msg:"Register not found"})
+    }
+
+    const UpdatedProject = {
+        title: data.title,
+        owner: data.owner,
+        technologies: data.technologies,
+        id: Number(req.params.id)
+    }
+
+    projects[ProjectIndex] = UpdatedProject
+
+    return res.json({data: projects[ProjectIndex] , msg: "Project updated successfuly"})
+
+})
+app.delete('/projects/:id', (req, res)=>{
+    const ProjectIndex = projects.findIndex(project => project.id == req.params.id)
+    
+    if(ProjectIndex === -1){
+        return res.status(401).json({msg:"Register not found"})
+    }
+    
+    projects.splice(ProjectIndex, 1)
+    return res.json({msg: "Register deleted successfuly"}).status(204)
+
+})
+
+
+
+// app.liste recebe dois parâmetros: a porta e a função callback que irá rodar quando o servidor ir para o ar
+app.listen(port, ()=>(console.log("✌ Servidor rodando na porta 8081")))
